5583ed62f6c0389b50aba99dcb745265c7adc46b
John Romani <john@anthood.com>
2022-07-27 23:31:35 +0300

    sitemap2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://thaireading.com",
+  experimental: {
+    integrations: true,
+  },
   integrations: [sitemap()],
   vite: {
     ssr: {
